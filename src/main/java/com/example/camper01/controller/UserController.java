5e72d56f2a1dfd7aac5de22beb95fad47f3b9107
leesh <tmdghks384@naver.com>
2022-06-07 20:44:57 +0900

    유저 회원가입 컨트롤러

--- src/main/java/com/example/camper01/controller/UserController.java
+++ src/main/java/com/example/camper01/controller/UserController.java
@@ -1,19 +1,36 @@
 package com.example.camper01.controller;
 
+import com.example.camper01.domain.UserVO;
+import com.example.camper01.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
-@RequestMapping("/")
 @RequiredArgsConstructor
 @Log4j2
 public class UserController {
+    private final UserService user;
 
-    @GetMapping
-    public String main(){
+    @GetMapping("/")
+    public String main() {
         return "user/login";
+    }
+
+    @GetMapping("/user/join")
+    public String join() {
+        return "user/join";
+    }
+
+    @GetMapping("/user/myPage/{userNum}")
+    public String myPage(@PathVariable("userNum") Long userNum, Model model) {
+        model.addAttribute("user", user.readUser(userNum));
+        return "user/myPage";
     }
 }
--- src/main/java/com/example/camper01/controller/UserRestController.java
+++ src/main/java/com/example/camper01/controller/UserRestController.java
@@ -1,4 +1,38 @@
 package com.example.camper01.controller;
 
+import com.example.camper01.domain.UserVO;
+import com.example.camper01.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/user/rest/*")
+@RequiredArgsConstructor
+@Log4j2
 public class UserRestController {
+    private final UserService user;
+
+    @PostMapping("/join")
+    public ResponseEntity<String> insertUser(@RequestBody UserVO userVO, HttpServletRequest request) {
+        boolean isInserted = user.insertUser(userVO);
+        try {
+            if (isInserted == true) {
+                log.info("회원가입 성공");
+            }
+        } catch (DataAccessException e) {
+            log.warn("데이터 처리 실패");
+        } catch (Exception e) {
+            log.warn("시스템 에러");
+        }
+        return new ResponseEntity<>("회원가입이 완료되었습니다.", HttpStatus.OK);
+    }
 }
